c2cb752b18177a184391a2209c08250a0ccd1313
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-05 14:49:00 +0200

    chore: reshape src

--- scripts/build-compile.sh
+++ scripts/build-compile.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=scripts/.env
 . "$SCRIPT_DIR"/.env
 
-cd "$SCRIPT_DIR"/../src || exit 1
+cd "$SCRIPT_DIR"/../src/Functions || exit 1
 dotnet build
