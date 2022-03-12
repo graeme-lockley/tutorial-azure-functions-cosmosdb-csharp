9f044fd0baf30109f67c6bb731ff75b1192fe9f2
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-12 16:01:47 +0200

    refactor: simply getting key

--- scripts/build-database-content.sh
+++ scripts/build-database-content.sh
@@ -5,10 +5,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=scripts/.env
 . "$SCRIPT_DIR"/.env
 
-KEY=$(az cosmosdb keys list --name "$COSMOS_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query primaryMasterKey)
-KEY1=${KEY:1}
-
-export COSMOSDB_PRIMARY_KEY="${KEY1%?}"
+COSMOSDB_PRIMARY_KEY=$(az cosmosdb keys list --name "$COSMOS_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query primaryMasterKey --output tsv)
+export COSMOSDB_PRIMARY_KEY
 
 cd "$SCRIPT_DIR"/../src/CLI || exit 1
 dotnet run
--- scripts/configure-functions.sh
+++ scripts/configure-functions.sh
@@ -5,10 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=scripts/.env
 . "$SCRIPT_DIR"/.env
 
-KEY=$(az cosmosdb keys list --name "$COSMOS_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query primaryMasterKey)
-KEY1=${KEY:1}
-
-export COSMOSDB_PRIMARY_KEY="${KEY1%?}"
+COSMOSDB_PRIMARY_KEY=$(az cosmosdb keys list --name "$COSMOS_ACCOUNT_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query primaryMasterKey --output tsv)
 
 az deployment group create \
     --name "tafcccf" \
