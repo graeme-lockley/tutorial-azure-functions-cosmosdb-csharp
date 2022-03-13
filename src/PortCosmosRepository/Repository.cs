1a483f80ed5773dcb781fe7621da1ac0ab869c69
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-13 12:52:48 +0200

    feature: build out the Cosmos repository port

---
+++ src/PortCosmosRepository/Repository.cs
@@ -0,0 +1,75 @@
+﻿namespace PortCosmosRepository;
+
+using Microsoft.Azure.Cosmos;
+using System.Net;
+
+using CoreLibrary.Entity;
+using CoreLibrary.Ports.Out;
+
+public class Repository : IRepository
+{
+    private Connection Connection;
+
+    public Repository(Connection connection)
+    {
+        this.Connection = connection;
+    }
+
+    public async Task<Friend> AddFriend(string lastName, string firstName, string? knownAs)
+    {
+        Guid id = Guid.NewGuid();
+        Friend friend = new Friend(id.ToString(), lastName, firstName, knownAs);
+
+        try
+        {
+            var container = await Connection.Container();
+
+            ItemResponse<Friend> friendResponse = await container.CreateItemAsync<Friend>(friend, new PartitionKey(lastName));
+            LogInformation("Created item in database with id: {0} Operation consumed {1} RUs.\n", friendResponse.Resource.Id, friendResponse.RequestCharge);
+        }
+        catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.Conflict)
+        {
+            LogInformation("Item in database with id: {0} already exists\n", friend.Id);
+        }
+
+        return friend;
+    }
+
+    public async Task<Friend?> GetFriend(string id)
+    {
+        var result = await Query($"SELECT * FROM c WHERE c.id = '{id}'");
+
+        return (result.Count > 0) ? result[0] : null;
+    }
+
+    public Task<List<Friend>> FindFriendOnLastName(string lastName) =>
+        Query($"SELECT * FROM c WHERE c.LastName = '{lastName}'");
+
+    private async Task<List<Friend>> Query(string sqlQueryText)
+    {
+        LogInformation("Running query: {0}\n", sqlQueryText);
+
+        QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+        var container = await Connection.Container();
+        FeedIterator<Friend> queryResultSetIterator = container.GetItemQueryIterator<Friend>(queryDefinition);
+
+        List<Friend> friends = new List<Friend>();
+
+        while (queryResultSetIterator.HasMoreResults)
+        {
+            FeedResponse<Friend> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+            foreach (Friend friend in currentResultSet)
+            {
+                friends.Add(friend);
+                LogInformation("\tRead {0}\n", friend);
+            }
+        }
+
+        return friends;
+    }
+
+    private void LogInformation(string message, params object?[] args)
+    {
+        Connection.LogInformation(message, args);
+    }
+}
