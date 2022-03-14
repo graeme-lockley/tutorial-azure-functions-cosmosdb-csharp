4267c01290b262a0f87260f3e90bf787d7c3c8e0
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-14 17:41:58 +0200

    refactor: move the repository test out into its own package

--- src/PortInMemoryRepositoryTest/RepositoryTest.cs
+++ src/PortInMemoryRepositoryTest/RepositoryTest.cs
@@ -3,53 +3,14 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
+using CoreLibrary.Ports.Out;
 using PortInMemoryRepository;
+using PortRepositoryTest;
 
 [TestClass]
 public class RepositoryTest : AbstractRepositoryTest
 {
-    public override async Task<Repository> newRepository() =>
+    public override async Task<IRepository> newRepository() =>
         await Task.Run(() => new Repository());
 }
 
-public abstract class AbstractRepositoryTest
-{
-    private Repository? repository;
-
-    abstract public Task<Repository> newRepository();
-
-    [TestInitialize]
-    public async Task Init()
-    {
-        repository = await newRepository();
-        await repository.Truncate();
-    }
-
-    [TestMethod]
-    public async Task GivenRepository_WhenTruncate_ItHasNoEntries()
-    {
-        var count = await repository!.Count();
-
-        Assert.AreEqual(count, 0);
-    }
-
-    [TestMethod]
-    public async Task GivenRepository_WhenGetFriendOnUnknownID_ReturnsNull()
-    {
-        var friend = await repository!.GetFriend(" ");
-
-        Assert.IsNull(friend);
-    }
-
-    [TestMethod]
-    public async Task GivenRepository_WhenGetFriendOnKnownID_ReturnsFriend()
-    {
-        var friend = await repository!.AddFriend("Smith", "Robert", null);
-        var newFriend = await repository.GetFriend(friend.Id)!;
-
-        Assert.AreEqual(friend.Id, newFriend.Id);
-        Assert.AreEqual(friend.LastName, newFriend.LastName);
-        Assert.AreEqual(friend.FirstName, newFriend.FirstName);
-        Assert.AreEqual(friend.KnownAs, newFriend.KnownAs);
-    }
-}
---
+++ src/PortRepositoryTest/PortRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿namespace PortRepositoryTest;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+using CoreLibrary.Ports.Out;
+
+public abstract class AbstractRepositoryTest
+{
+    private IRepository? repository;
+
+    abstract public Task<IRepository> newRepository();
+
+    [TestInitialize]
+    public async Task Init()
+    {
+        repository = await newRepository();
+        await repository.Truncate();
+    }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenTruncate_ItHasNoEntries()
+    {
+        var count = await repository!.Count();
+
+        Assert.AreEqual(count, 0);
+    }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenGetFriendOnUnknownID_ReturnsNull()
+    {
+        var friend = await repository!.GetFriend(" ");
+
+        Assert.IsNull(friend);
+    }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenGetFriendOnKnownID_ReturnsFriend()
+    {
+        var friend = await repository!.AddFriend("Smith", "Robert", null);
+        var newFriend = (await repository.GetFriend(friend.Id))!;
+
+        Assert.AreEqual(friend.Id, newFriend.Id);
+        Assert.AreEqual(friend.LastName, newFriend.LastName);
+        Assert.AreEqual(friend.FirstName, newFriend.FirstName);
+        Assert.AreEqual(friend.KnownAs, newFriend.KnownAs);
+    }
+}
