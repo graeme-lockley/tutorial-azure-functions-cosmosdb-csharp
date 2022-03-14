23f73c6cc5c67b889198e7c31a84ec7fc5a1065b
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-14 17:16:14 +0200

    test: add more tests against IRepository

--- src/PortInMemoryRepositoryTest/RepositoryTest.cs
+++ src/PortInMemoryRepositoryTest/RepositoryTest.cs
@@ -14,16 +14,42 @@ public class RepositoryTest : AbstractRepositoryTest
 
 public abstract class AbstractRepositoryTest
 {
+    private Repository? repository;
+
     abstract public Task<Repository> newRepository();
+
+    [TestInitialize]
+    public async Task Init()
+    {
+        repository = await newRepository();
+        await repository.Truncate();
+    }
 
     [TestMethod]
     public async Task GivenRepository_WhenTruncate_ItHasNoEntries()
     {
-        var repository = await newRepository();
-
-        await repository.Truncate();
-        var count = await repository.Count();
+        var count = await repository!.Count();
 
         Assert.AreEqual(count, 0);
+    }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenGetFriendOnUnknownID_ReturnsNull()
+    {
+        var friend = await repository!.GetFriend(" ");
+
+        Assert.IsNull(friend);
+    }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenGetFriendOnKnownID_ReturnsFriend()
+    {
+        var friend = await repository!.AddFriend("Smith", "Robert", null);
+        var newFriend = await repository.GetFriend(friend.Id)!;
+
+        Assert.AreEqual(friend.Id, newFriend.Id);
+        Assert.AreEqual(friend.LastName, newFriend.LastName);
+        Assert.AreEqual(friend.FirstName, newFriend.FirstName);
+        Assert.AreEqual(friend.KnownAs, newFriend.KnownAs);
     }
 }
