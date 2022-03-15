144e6a7f7838eab8571e8f1d46a54cc9311fc504
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-15 05:27:39 +0200

    test: add findOnLastName test

--- src/PortRepositoryTest/PortRepositoryTest.cs
+++ src/PortRepositoryTest/PortRepositoryTest.cs
@@ -45,4 +45,17 @@ public abstract class AbstractRepositoryTest
         Assert.AreEqual(friend.FirstName, newFriend.FirstName);
         Assert.AreEqual(friend.KnownAs, newFriend.KnownAs);
     }
+
+    [TestMethod]
+    public async Task GivenRepository_WhenFindOnLastName_ReturnsMatches()
+    {
+        await repository!.AddFriend("Smith", "Robert", null);
+        await repository.AddFriend("Lockley", "Graeme", null);
+        await repository.AddFriend("Jones", "Frank", null);
+        await repository.AddFriend("Lockley", "David", null);
+
+        Assert.AreEqual((await repository.FindFriendOnLastName("Lockley")).Count, 2);
+        Assert.AreEqual((await repository.FindFriendOnLastName("Jones")).Count, 1);
+        Assert.AreEqual((await repository.FindFriendOnLastName("Williams")).Count, 0);
+    }
 }
