c11af89e166494648855cca57597c621cecf0c27
Graeme Lockley <graeme.lockley@gmail.com>
2022-05-02 10:11:06 +0200

    feature: add support for cosmos regionName location

--- src/infra/src/handlers/azure__cosmos__create.ts
+++ src/infra/src/handlers/azure__cosmos__create.ts
@@ -1,22 +1,33 @@
 import { exec } from "../exec.ts";
-import { IAction, ILintResult, lintFieldNotUndefinedNotEmpty } from "./declarations.ts";
+import {
+  IAction,
+  ILintResult,
+  lintFieldNotEmpty,
+  lintFieldNotUndefinedNotEmpty,
+} from "./declarations.ts";
 
 const handlerName = "azure/cosmos/create";
 
 interface IHandlerAction extends IAction {
   name?: string | undefined;
   rg?: string | undefined;
+  location?: string | undefined;
 }
 
 const lint = (result: Array<ILintResult>, action: IHandlerAction): void => {
   lintFieldNotUndefinedNotEmpty(action.id, handlerName, "id", result);
   lintFieldNotUndefinedNotEmpty(action.name, handlerName, "name", result);
   lintFieldNotUndefinedNotEmpty(action.rg, handlerName, "rg", result);
+  lintFieldNotEmpty(action.location, handlerName, "location", result);
 };
 
 const run = async (action: IHandlerAction): Promise<void> => {
+  const locationSuffix = action.location === undefined
+    ? ""
+    : ` --locations "regionName=${action.location}"`;
+
   const command =
-    `az cosmosdb create --name "${action.name}" --resource-group "${action.rg}"`;
+    `az cosmosdb create --name "${action.name}" --resource-group "${action.rg}"${locationSuffix}`;
 
   await exec(command, handlerName, action.id, action.name);
 };
