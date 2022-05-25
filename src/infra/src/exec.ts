b453c4b2802662671ebd3e6445716ec3041426fd
Graeme Lockley <graeme.lockley@gmail.com>
2022-05-25 06:40:42 +0200

    feature: support for exec options

--- src/infra/src/exec.ts
+++ src/infra/src/exec.ts
@@ -2,34 +2,58 @@ import { exec as execLib } from "https://deno.land/x/garn_exec@0.4.2/exec.js";
 
 import { failOnError } from "./logging.ts";
 
+export type ExecOptions = {
+  showScript?: boolean;
+  showOutput?: boolean;
+  exitOnError: boolean;
+};
+
+export const defaultExecOptions: ExecOptions = {
+  showScript: true,
+  showOutput: false,
+  exitOnError: true,
+};
+
 export const exec = async (
   command: string,
   handlerName: string,
   id: string,
   name: string | undefined = undefined,
+  options: ExecOptions | undefined = undefined,
 ): Promise<string> => {
-  console.log(`${handlerName}: ${command}`);
+  const opts = Object.assign({}, defaultExecOptions, options);
+
+  if (opts.showScript) {
+    console.log(`${handlerName}: ${command}`);
+  }
 
   try {
     const result = await execLib(command);
-    if (result.code === 0 && result.success) {
+
+    if (opts.showOutput) {
       console.info(result.stdout, result.stderr);
-      return (result.stdout + result.stderr).trim();
-    } else {
+    }
+
+    if (opts.exitOnError && (result.code !== 0 || !result.success)) {
       failOnError(
         `Error: id=${id}, name=${name}: Action returned error:`,
         result.stdout,
         result.stderr,
       );
-      return "";
+      Deno.exit(1);
     }
+
+    return (result.stdout + result.stderr).trim();
   } catch (error) {
-    failOnError(
-      `Error: id=${id}, name=${name}: Action returned error:`,
-      error.stdout,
-      error.stderr,
-    );
-    return "";
+    if (opts.exitOnError) {
+      failOnError(
+        `Error: id=${id}, name=${name}: Action returned error:`,
+        error.stdout,
+        error.stderr,
+      );
+    }
+
+    return (error.stdout + error.stderr).trim();
   }
 };
 
