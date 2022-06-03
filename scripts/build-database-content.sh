c5b1b15d098d7941114c2c40458db9c61603b46e
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-03 06:17:52 +0200

    feature: use RBAC

--- scripts/build-database-content.sh
+++ scripts/build-database-content.sh
@@ -5,8 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=scripts/.env
 . "$SCRIPT_DIR"/.env
 
-COSMOSDB_PRIMARY_KEY=$(az cosmosdb keys list --name "$COSMOS_ACCOUNT_NAME" --resource-group "$STORE_RESOURCE_GROUP_NAME" --query primaryMasterKey --output tsv)
-export COSMOSDB_PRIMARY_KEY
-
 cd "$SCRIPT_DIR"/../src/CLI || exit 1
 dotnet run
