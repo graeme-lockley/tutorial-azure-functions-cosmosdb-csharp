261aa47a6edaf3f3f75f63aff1ecf51cee6d4a31
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-04 09:34:42 +0200

    chore: dump log file as helps with debugging

--- scripts/configure-test-environment.sh
+++ scripts/configure-test-environment.sh
@@ -23,4 +23,4 @@ echo "Resource Group Name: $RESOURCE_GROUP_NAME"
 echo "Cosmos Account Name: $COSMOS_ACCOUNT_NAME"
 
 cd "$SCRIPT_DIR"/../src/infra || exit 1
-./infra-runner.ts run --no-log "$SCRIPT_DIR"/../src/infra/test-environment-setup.yaml
+./infra-runner.ts run "$SCRIPT_DIR"/../src/infra/test-environment-setup.yaml
