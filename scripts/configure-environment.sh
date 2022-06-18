0788403e672f964d68fc0ddb127dd4e41c0dda0d
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-18 11:44:32 +0200

    refactor: call infra-runner

--- scripts/configure-environment.sh
+++ scripts/configure-environment.sh
@@ -2,14 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# shellcheck source=scripts/.env
-. "$SCRIPT_DIR"/.env
-
-az deployment sub create \
-    --name "tafcc-configure-full-environment" \
-    --location "$COMPUTE_RESOURCE_GROUP_LOCATION" \
-    --template-file "${SCRIPT_DIR}/../infra/full-environment.bicep" \
-    --parameters "computeResourceGroupName=$COMPUTE_RESOURCE_GROUP_NAME" \
-    --parameters "computeResourceGroupLocation=$COMPUTE_RESOURCE_GROUP_LOCATION" \
-    --parameters "storeResourceGroupName=$STORE_RESOURCE_GROUP_NAME" \
-    --parameters "storeResourceGroupLocation=$STORE_RESOURCE_GROUP_LOCATION"
+cd "$SCRIPT_DIR"/../src/infra || exit 1
+./infra-runner.ts run ./environment-setup.yaml
