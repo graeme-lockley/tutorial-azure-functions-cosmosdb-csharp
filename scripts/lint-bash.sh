7b0671a7f4e4e9e533235333a44b5dc2019468df
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-26 10:35:02 +0200

    fix: hmmmmm... pipeline runner has different version

--- scripts/lint-bash.sh
+++ scripts/lint-bash.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . "$SCRIPT_DIR"/.env
 
-shellcheck -e SC1091 "$SCRIPT_DIR"/../scripts/*.sh || exit 1
-shellcheck -e SC1091 "$SCRIPT_DIR"/../infra/env/*.sh "$SCRIPT_DIR"/../infra/env/*.up "$SCRIPT_DIR"/../infra/env/*.down || exit 1
-shellcheck -e SC1091 "$SCRIPT_DIR"/../infra/test-setup/*.sh "$SCRIPT_DIR"/../infra/test-setup/*.up || exit 1
+shellcheck -e SC1090,SC1091 "$SCRIPT_DIR"/../scripts/*.sh || exit 1
+shellcheck -e SC1090,SC1091 "$SCRIPT_DIR"/../infra/env/*.sh "$SCRIPT_DIR"/../infra/env/*.up "$SCRIPT_DIR"/../infra/env/*.down || exit 1
+shellcheck -e SC1090,SC1091 "$SCRIPT_DIR"/../infra/test-setup/*.sh "$SCRIPT_DIR"/../infra/test-setup/*.up || exit 1
