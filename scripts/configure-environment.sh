860b28b35adefc464ae2e3c7a9ce4e6dde0fc70e
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-26 10:32:19 +0200

    chore: use the script runner

--- scripts/configure-environment.sh
+++ scripts/configure-environment.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd "$SCRIPT_DIR"/../src/infra || exit 1
-./infra-runner.ts run ./environment-setup.yaml
+cd "$SCRIPT_DIR"/../infra/env || exit 1
+"$SCRIPT_DIR"/infra-runner.sh run
