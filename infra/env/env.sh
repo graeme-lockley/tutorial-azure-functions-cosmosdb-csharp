9049e8a27e5b536526055294256c583fdb7f35c3
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-26 11:43:12 +0200

    fix: set PRINCIPAL_ID if none is provided

--- infra/env/env.sh
+++ infra/env/env.sh
@@ -4,4 +4,8 @@ export STORE_RG_NAME=tutorialAzureFuncCosmosCSS
 export COMPUTE_RG_NAME=tutorialAzureFuncCosmosCSC
 
 export ACCOUNT_NAME=tafccdb
-export PRINCIPAL_ID=22395703-af19-49be-a81f-ba230562fce3
+
+if [ -z "$PRINCIPAL_ID" ]
+then
+    export PRINCIPAL_ID=22395703-af19-49be-a81f-ba230562fce3
+fi
