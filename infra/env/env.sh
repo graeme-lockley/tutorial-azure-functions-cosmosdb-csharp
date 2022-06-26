6db9bc5a5055709d0541a03d14be80d5f7817cbf
Graeme Lockley <graeme.lockley@gmail.com>
2022-06-26 13:30:02 +0200

    feature: create app insights

--- infra/env/env.sh
+++ infra/env/env.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 export STORE_RG_NAME=tutorialAzureFuncCosmosCSS
+export STORE_RG_LOCATION=centralus
 export COMPUTE_RG_NAME=tutorialAzureFuncCosmosCSC
+export COMPUTE_RG_LOCATION=westus
 
 export ACCOUNT_NAME=tafccdb
 
 export FUNCTION_APP_NAME=tafcc
 export FUNCTION_APP_STORAGE_NAME=tafccs
+
+export APP_INSIGHTS_NAME=tafccn
 
 if [ -z "$PRINCIPAL_ID" ]
 then
