fddc1e0ef20b6b502e36d55f2b08efc666c5ced9
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-10 09:04:12 +0200

    refactor: remove as not used anymore

--- scripts/build-compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-. "$SCRIPT_DIR"/.env
-
-cd "$SCRIPT_DIR"/../src/Functions || exit 1
-dotnet build
--- scripts/build-database-content.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-. "$SCRIPT_DIR"/.env
-
-cd "$SCRIPT_DIR"/../src/CLI || exit 1
-dotnet run
